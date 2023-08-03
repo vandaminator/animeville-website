1629645efba5858baccf689fd5c37aac2bfc6805
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2023-08-04 00:27:22 +0200

    update improve search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { Season } from "@/utils/Anilist/types";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { AdvancedSearch, Result } from "@/types/Anilist/AdvancedAnimeSearch";
 import SearchLoading from "./loading";
 import AnimeContainer from "@/components/AnimeContainer";
@@ -14,7 +14,7 @@ function SearchPage() {
   const [content, setContent] = useState<Result[] | "loading">("loading");
   const [appState, setAppState] = useState({ nextPage: 2, isLast: false });
 
-  useEffect(() => {
+  const getSearchParams = useCallback(() => {
     const newParams: Record<string, string> = {};
 
     const query = searchParams.get("query");
@@ -33,30 +33,34 @@ function SearchPage() {
     if (genres) {
       newParams.genres = genres;
     }
-    setParams(newParams);
+
+    return newParams;
   }, [searchParams]);
 
   useEffect(() => {
     setContent("loading");
     setAppState({ nextPage: 2, isLast: false });
 
+    const newParams = getSearchParams();
+    setParams(newParams);
+
     const loadData = async () => {
-      const strParams = new URLSearchParams(params).toString();
-      console.log(strParams)
+      const strParams = new URLSearchParams(newParams).toString();
+      console.log(strParams);
       const url = `https://consum-net-api.vercel.app/meta/anilist/advanced-search?${strParams}`;
       const response = await fetch(url);
       if (!response.ok) throw new Error("Search is unavaliable Today");
       const data: AdvancedSearch = await response.json();
-      console.log(data)
       setAppState((prev) => ({ ...prev, isLast: !data.hasNextPage }));
 
       setContent(data.results);
     };
     loadData();
-  }, [params]);
+  }, [getSearchParams, searchParams]);
 
   const loadMore = async () => {
-    const searchObj = { ...params, page: appState.nextPage.toString() };
+    const newParams = getSearchParams();
+    const searchObj = { ...newParams, page: appState.nextPage.toString() };
     const strParams = new URLSearchParams(searchObj).toString();
     const url = `https://consum-net-api.vercel.app/meta/anilist/advanced-search?${strParams}`;
 
@@ -89,14 +93,20 @@ function SearchPage() {
               Search results for &quot; {params.query} &quot;
             </h1>
           )}
-          <AnimeContainer content={content} contentType="card" />
-          <div
-            className="w-full cursor-pointer rounded-lg bg-lightjetblack/75 p-3 hover:bg-lightjetblack hover:text-gold active:bg-lightjetblack active:text-gold"
-            style={{ display: appState.isLast ? "none" : "flex" }}
-            onClick={loadMore}
-          >
-            <AiOutlineArrowDown size={"24px"} className="mx-auto" />
-          </div>
+          {content.length !== 0 ? (
+            <>
+              <AnimeContainer content={content} contentType="card" />
+              <div
+                className="w-full cursor-pointer rounded-lg bg-lightjetblack/75 p-3 hover:bg-lightjetblack hover:text-gold active:bg-lightjetblack active:text-gold"
+                style={{ display: appState.isLast ? "none" : "flex" }}
+                onClick={loadMore}
+              >
+                <AiOutlineArrowDown size={"24px"} className="mx-auto" />
+              </div>
+            </>
+          ) : (
+            <h1 className="my-10 text-2xl">No results</h1>
+          )}
         </>
       )}
     </div>
